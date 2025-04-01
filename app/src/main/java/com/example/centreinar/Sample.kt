20e5e5b2574120977cbb18590707be5de8e80b9d
Lucas Mombach <lucasmmombach@gmail.com>
2025-04-01 11:23:05 -0300

    turns Sample into a Room entity

--- app/src/main/java/com/example/centreinar/Sample.kt
+++ app/src/main/java/com/example/centreinar/Sample.kt
@@ -1,4 +1,7 @@
 package com.example.centreinar
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /*
 Esverdeado → Greenish
@@ -11,17 +14,53 @@ Germinados → Germinated
 Imaturos (chochos) → Immature (shrivelled)
 Danificados por insetos → Insect-damaged
 Gessado → starchy
+Matérias estranhas e Impurezas → foreign matter and Impurities
  */
+
+@Entity(tableName = "sample")
 data class Sample(
-    val id: Int,
-    val weight: Float,
-    val humidity: Float,
-    val greenish: Float,
-    val brokenCrackedDamaged: Float,
-    val burnt: Float,
-    val sour: Float,
-    val moldy: Float,
-    val fermented: Float,
-    val germinated: Float,
-    val immature: Float
-)
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "grain")
+    val grain: String ="",
+
+    @ColumnInfo(name = "group")
+    val group: Int = 0,
+
+    @ColumnInfo(name = "weight")
+    val weight: Float = 0.0f,
+
+    @ColumnInfo(name = "foreignMattersAndImpurities")
+    val foreignMattersAndImpurities: Float = 0.0f,
+
+    @ColumnInfo(name = "humidity")
+    val humidity: Float = 0.0f,
+
+    @ColumnInfo(name = "greenish")
+    val greenish: Float = 0.0f,
+
+    @ColumnInfo(name = "brokenCrackedDamaged")
+    val brokenCrackedDamaged: Float = 0.0f,
+
+    @ColumnInfo(name = "burnt")
+    val burnt: Float = 0.0f,
+
+    @ColumnInfo(name = "sour")
+    val sour: Float = 0.0f,
+
+    @ColumnInfo(name = "moldy")
+    val moldy: Float = 0.0f,
+
+    @ColumnInfo(name = "fermented")
+    val fermented: Float = 0.0f,
+
+    @ColumnInfo(name = "germinated")
+    val germinated: Float = 0.0f,
+
+    @ColumnInfo(name = "immature")
+    val immature: Float = 0.0f
+) {
+    // Secondary constructor with default values
+    constructor() : this(0, " ", 0, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f)
+}
