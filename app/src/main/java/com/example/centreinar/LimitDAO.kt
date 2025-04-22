f7507745e6fdb4badbb868f0d4d2c6816626db0f
Lucas Mombach <lucasmmombach@gmail.com>
2025-04-22 14:49:29 -0300

    add a getLimitByID, and function that get limits of individual defects can be selected by source

--- app/src/main/java/com/example/centreinar/LimitDAO.kt
+++ app/src/main/java/com/example/centreinar/LimitDAO.kt
@@ -18,24 +18,34 @@ interface LimitDao{
     @Update
     suspend fun updateLimit(limit: Limit)
 
+    @Query("SELECT * FROM limits WHERE id = :id")
+    suspend fun getLimitById(id:Long): List<Limit>
+
     @Query("SELECT * FROM limits WHERE grain = :grain")
     suspend fun getLimitsByGrain(grain:String): List<Limit>
 
-    @Query("SELECT type,brokenCrackedDamagedLowerLim AS lowerL ,brokenCrackedDamagedUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForBrokenCrackedDamaged(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT * FROM limits WHERE grain = :grain AND source = :limitSource AND `group` = :group")
+    suspend fun getLimitsBySource(grain:String,group: Int,limitSource:Int): List<Limit>
 
-    @Query("SELECT type,greenishLowerLim AS lowerL,greenishUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForGreenish(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT * FROM limits WHERE grain = :grain AND type = :tipo AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsByType(grain:String,group: Int,tipo:Int, limitSource: Int): Limit
 
-    @Query("SELECT type, burntLowerLim AS lowerL, burntUpLim  AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForBurnt(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT type,brokenCrackedDamagedLowerLim AS lowerL ,brokenCrackedDamagedUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForBrokenCrackedDamaged(grain:String,group:Int,limitSource: Int):List<LimitCategory>
 
-    @Query("SELECT type,moldyLowerLim AS lowerL ,moldyUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForMoldy(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT type,greenishLowerLim AS lowerL,greenishUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForGreenish(grain:String,group:Int,limitSource: Int):List<LimitCategory>
 
-    @Query("SELECT type, burntOrSourLowerLim AS lowerL, burntOrSourUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForBurntOrSour(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT type, burntLowerLim AS lowerL, burntUpLim  AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForBurnt(grain:String,group:Int,limitSource: Int):List<LimitCategory>
 
-    @Query("SELECT type,spoiledTotalLowerLim AS lowerL ,spoiledTotalUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group")
-    suspend fun getLimitsForSpoiledTotal(grain:String,group:Int):List<LimitCategory>
+    @Query("SELECT type,moldyLowerLim AS lowerL ,moldyUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForMoldy(grain:String,group:Int,limitSource: Int):List<LimitCategory>
+
+    @Query("SELECT type, burntOrSourLowerLim AS lowerL, burntOrSourUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForBurntOrSour(grain:String,group:Int,limitSource: Int):List<LimitCategory>
+
+    @Query("SELECT type,spoiledTotalLowerLim AS lowerL ,spoiledTotalUpLim AS upperL FROM limits WHERE grain = :grain AND `group` = :group AND source = :limitSource")
+    suspend fun getLimitsForSpoiledTotal(grain:String,group:Int,limitSource: Int):List<LimitCategory>
+
 }
