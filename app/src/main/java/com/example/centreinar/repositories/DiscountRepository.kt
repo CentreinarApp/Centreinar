a6e11cd44cd03fe314775cf3cadfbd1fe89a8aed
Lucas Mombach <lucasmmombach@gmail.com>
2025-04-22 14:37:09 -0300

    add Discount Repository to calculate the discount using limits inserted by the user and the official table

---
+++ app/src/main/java/com/example/centreinar/repositories/DiscountRepository.kt
@@ -0,0 +1,18 @@
+package com.example.centreinar.repositories
+
+import androidx.core.view.ContentInfoCompat.Source
+import com.example.centreinar.Discount
+import com.example.centreinar.Sample
+import com.example.centreinar.Limit
+import com.example.centreinar.InputDiscount
+
+interface DiscountRepository{
+    suspend fun calculateDiscount(grain:String, group:Int, tipo:Int, sample: InputDiscount,doesTechnicalLoss:Boolean,doesClassificationLoss:Boolean, doesDeduction:Boolean): Long
+    suspend fun calculateDiscount(grain:String, group:Int, tipo:Int, sample: InputDiscount, limit: Map<String,Float>, doesTechnicalLoss:Boolean,doesClassificationLoss:Boolean, doesDeduction:Boolean): Long
+    suspend fun calculateTechnicalLoss(storageDays: Int, humidityAndImpuritiesLoss:Float, lotWeight:Float):Float
+    suspend fun calculateDeduction(deductionValue:Float,classificationLoss:Float):Float
+    suspend fun getDiscountById(id: Long): Discount?
+    suspend fun getLimitsByType(grain:String, group:Int, tipo:Int,limitSource: Int):Map<String,Float>
+}
+
+//implement a way of creating custom limits
