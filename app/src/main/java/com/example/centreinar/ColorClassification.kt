01779539208d418324a47b0a20b23ae3e196737d
Lucas Mombach <lucasmmombach@gmail.com>
2025-05-13 16:38:54 -0300

    create a color classification entity

---
+++ app/src/main/java/com/example/centreinar/ColorClassification.kt
@@ -0,0 +1,21 @@
+package com.example.centreinar
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ColorClassification")
+data class ColorClassification(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name = "grain")
+    val grain: String ="",
+    @ColumnInfo(name = "classificationId")
+    val classificationId:Int,
+    @ColumnInfo(name = "yellowPercentage")
+    val yellowPercentage:Float,
+    @ColumnInfo(name = "otherColorPercentage")
+    val otherColorPercentage:Float,
+    @ColumnInfo(name = "class")
+    val framingClass: String ="",
+)
