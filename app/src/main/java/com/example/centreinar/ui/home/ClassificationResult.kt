e9cd5cb2fbf6795effcdeb08c0490edc9ab447f4
Lucas Mombach <lucasmmombach@gmail.com>
2025-06-05 22:00:20 -0300

    add screen to present the result of classification, including color class results and observations

---
+++ app/src/main/java/com/example/centreinar/ui/home/ClassificationResult.kt
@@ -0,0 +1,117 @@
+package com.example.centreinar.ui.home
+
+import ClassificationTable
+import android.util.Log
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.centreinar.ColorClassification
+
+@Composable
+fun ClassificationResult(
+    navController: NavController,
+    viewModel: HomeViewModel= hiltViewModel()
+){
+    val classification by viewModel.classification.collectAsState()
+    val isLoading by viewModel.isLoading.collectAsState()
+    val error by viewModel.error.collectAsState()
+    //val doesDefineColorClass = viewModel.doesDefineColorClass
+
+    var colorClassificationResult by remember { mutableStateOf<ColorClassification?>(null) }
+
+    // When doesDefineColorClass becomes true, call the suspend function
+    if (viewModel.doesDefineColorClass == true) {
+        LaunchedEffect(Unit) {
+            colorClassificationResult = viewModel.getClassColor()
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        when {
+            isLoading -> {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(32.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+
+            error != null -> {
+                Text(
+                    text = error!!,
+                    color = MaterialTheme.colorScheme.error,
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .fillMaxWidth()
+                )
+            }
+
+            classification != null -> {
+                ClassificationTable(
+                    classification = classification!!,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Spacer(Modifier.height(16.dp))
+
+                if (viewModel.doesDefineColorClass == true && colorClassificationResult != null) {
+                    ClassColorResult(
+                        colorClassificationResult!!,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+
+                val observation = viewModel.observation.toString()
+
+                ObservationCard(
+                    observation,
+                    modifier = Modifier.fillMaxWidth()
+                )
+
+                Log.e("Observations", "observations in screen:${observation}")
+                Spacer(Modifier.height(16.dp))
+
+                Button(
+                    onClick = {
+                        navController.navigate("home")
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("Nova Análise")
+
+                }
+            }
+        }
+    }
+}
