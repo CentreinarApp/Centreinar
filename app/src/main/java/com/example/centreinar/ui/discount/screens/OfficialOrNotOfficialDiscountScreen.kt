68aa6e32f46b4358e0ec5c2b43ff8035619afb73
Lucas Mombach <lucasmmombach@gmail.com>
2025-07-09 16:53:33 -0300

    add screens to select grain,group and type of limit of tolerance

---
+++ app/src/main/java/com/example/centreinar/ui/discount/screens/OfficialOrNotOfficialDiscountScreen.kt
@@ -0,0 +1,70 @@
+package com.example.centreinar.ui.discount.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import com.example.centreinar.ui.discount.viewmodel.DiscountViewModel
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+
+
+@Composable
+public fun OfficialOrNotOfficialScreen(
+    navController: NavController,
+    viewModel: DiscountViewModel = hiltViewModel()
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = {
+                viewModel.isOfficial = true
+                navController.navigate("limitInput") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .padding(vertical = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+            )
+        ) {
+            Text("Classificação Oficial", style = MaterialTheme.typography.titleMedium)
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+                viewModel.isOfficial = false
+                navController.navigate("limitInput")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(80.dp)
+                .padding(vertical = 8.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondaryContainer,
+                contentColor = MaterialTheme.colorScheme.onSecondaryContainer
+            )
+        ) {
+            Text("Classificação Não Oficial", style = MaterialTheme.typography.titleMedium)
+        }
+    }
+}
