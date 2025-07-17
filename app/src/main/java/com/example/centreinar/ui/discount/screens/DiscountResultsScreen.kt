c33f9c49e793ad36d899aef2bde0df2fa3e81fae
Lucas Mombach <lucasmmombach@gmail.com>
2025-07-17 16:30:18 -0300

    make it scrollable and add a button to return to the homescreen

--- app/src/main/java/com/example/centreinar/ui/discount/screens/DiscountResultsScreen.kt
+++ app/src/main/java/com/example/centreinar/ui/discount/screens/DiscountResultsScreen.kt
@@ -1,9 +1,21 @@
 package com.example.centreinar.ui.discount.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.centreinar.ui.discount.components.DiscountResultsTable
@@ -15,10 +27,32 @@ fun DiscountResultScreen(
     viewModel: DiscountViewModel = hiltViewModel()
 ){
     val discounts by viewModel.discounts.collectAsState()
-    if(discounts != null){
-        DiscountResultsTable(discounts!!)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(1.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if(discounts != null){
+            DiscountResultsTable(discounts!!)
+        }
+        else {
+            Text("Algo não deu certo")
+        }
+
+        Spacer(Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                navController.navigate("home")
+                viewModel.clearStates()
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Nova Análise")
+
+        }
     }
-    else {
-        Text("Algo não deu certo")
-    }
+
 }
