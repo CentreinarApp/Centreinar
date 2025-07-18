f3bab62a955d04bbb67d167e8e7cbef5c41f8de3
Lucas Mombach <lucasmmombach@gmail.com>
2025-07-18 16:02:12 -0300

    add technicalLoss in table of results

--- app/src/main/java/com/example/centreinar/ui/discount/components/DiscountResultsTable.kt
+++ app/src/main/java/com/example/centreinar/ui/discount/components/DiscountResultsTable.kt
@@ -81,6 +81,7 @@ fun DiscountResultsTable(
                 listOf(
                     Triple("Matéria Estranha e Impurezas", discounts.impuritiesLoss,discounts.impuritiesLossPrice),
                     Triple("Umidade", discounts.humidityLoss,discounts.humidityLossPrice),
+                    Triple("Quebra técnica", discounts.technicalLoss,discounts.technicalLossPrice),
                     Triple("Queimados", discounts.burntLoss, discounts.burntLossPrice),
                     Triple("Ardidos e Queimados", discounts.burntOrSourLoss, discounts.burntOrSourLossPrice),
                     Triple("Mofados", discounts.moldyLoss, discounts.moldyLossPrice),
