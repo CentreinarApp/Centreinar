2b606da84139da700481bbc8b6746a7488cd4257
Lucas Mombach <lucasmmombach@gmail.com>
2025-07-31 21:50:02 -0300

    hide detais with a switch

--- app/src/main/java/com/example/centreinar/ui/discount/screens/DiscountResultsScreen.kt
+++ app/src/main/java/com/example/centreinar/ui/discount/screens/DiscountResultsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.centreinar.ui.discount.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,10 +10,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -28,6 +33,8 @@ fun DiscountResultScreen(
     viewModel: DiscountViewModel = hiltViewModel()
 ){
     val discounts by viewModel.discounts.collectAsState()
+    var showMoreDetails by  remember { mutableStateOf(false) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -40,7 +47,17 @@ fun DiscountResultScreen(
 
             Spacer(Modifier.height(16.dp))
 
-            DiscountResultsTable(discounts!!)
+            Row {
+                Switch(
+                    checked = showMoreDetails,
+                    onCheckedChange = {showMoreDetails = it}
+                )
+                Text("Mais detalhes")
+            }
+
+            if(showMoreDetails){
+                DiscountResultsTable(discounts!!)
+            }
         }
         else {
             Text("Algo não deu certo")
